53378d0aeb05e4ca4823174824fc99512a7e33a5
Clever Tedeku <clevertedeku@gmail.com>
2021-04-15 19:38:06 +0200

    done

--- index.js
+++ index.js
@@ -17,11 +17,37 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    return self.connection.dropDatabase(); 
   })
+ 
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    return Recipe.create([{title: "potato Puree", level:"Easy Peasy", ingredients:"Potatoes", cuisine:"German"}])
+  })
+  .then((NewRecipe)=>{
+      console.log(NewRecipe.title)
+    return Recipe.insertMany(data)
+  })
+  .then((moreRecipes)=>{
+    moreRecipes.forEach((el)=>{
+      console.log(el.title) 
+    })
+   return Recipe.updateOne({title:"Rigatoni alla Genovese"}, {duration: "100"})
+  })
+  .then(()=>{
+    console.log("Rigatoni alla Genovese has been updated")
+   return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(()=>{
+    console.log("Carrot Cake has been deleted") 
+  })
+  .then(()=>{
+    mongoose.connection.close() 
+    console.log('disconnected')
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 
+
